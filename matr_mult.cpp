b07a947aa9c9e3c3a655e5d9cce9e0c70a9c8701
u49128 <yyk@intel-cluster>
2020-09-25 19:09:22 -0700

    updates

--- matr_mult.cpp
+++ matr_mult.cpp
@@ -2,18 +2,16 @@
 //
 //
 
-#include "timer.h"
-
-#include <iostream>
-#include <vector>
-#include <iterator>
-#include <algorithm>
+#include "cstdio"
 
 using namespace std;
 
-template <class T>
+//template <class T>
+typedef float T;
 void matr_mult(T const *A, T const *B, T *C, size_t N, size_t K, size_t M)
 {
+#pragma ivdep
+#pragma simd 
     for(size_t i = 0 ; i != N ; ++i) {
         for (size_t j = 0 ; j != M ; ++j) {
             T s = 0;
@@ -27,43 +25,72 @@ void matr_mult(T const *A, T const *B, T *C, size_t N, size_t K, size_t M)
     }
 }
 
-template<class T>
-void print_as_matr(vector<T> const &M, size_t rows, size_t colls, string title)
+//template<class T>
+void print_as_matr(T const *M, size_t rows, size_t colls, char const *title)
 {
-    if(!title.empty())
-        cout << title << endl;
+    if(title)
+        printf("%s\n", title);
     if(rows * colls > 100) {
-        cout << "... too big\n";
+        printf("... too big\n");
         return;
     }
         
     for(size_t i = 0 ; i != rows ; ++i) {
-        auto pos = M.begin() + i * colls;
-        copy(pos, pos + colls, ostream_iterator<T>(cout, " "));
-        cout << endl;
+        for(size_t j = 0 ; j != colls ; ++j) {
+            printf("%g ", (double)M[i * colls + j]);
+        }
     }
 }
 
+T *one(size_t n)
+{
+    T *data = new T[n];
+    data += n;
+    while(n--)
+        *--data = (T)1;
+    return data;
+}
+
+#include "timer.h"
 int main()
 {
     {
-        int N = 1000, K = 1000, M =1000;
+        const int N = 3, K = 3, M =3;
         
-        vector<float> A(N*K, 1);
+        float A[] = {1, 0, 0, 0, 2, 0, 0, 0, 3};
         print_as_matr(A, N, K, "A:");
-        vector<float> B(K*M, 1);
+        float B[] =  {1, 0, 0, 0, 1, 0, 0, 0, 1};
         print_as_matr(B, K, M, "B:");
         
-        vector<float> C(N*M);
+        float C[N*M];
 
         Timer t;
         t.start();
-        matr_mult(A.data(), B.data(), C.data(), N, K, M);
+        matr_mult(A, B, C, N, K, M);
         t.stop();
         print_as_matr(C, N, M, "C:");
-        cout << "Elapsed: " << t.elapsed() << "sec.\n";
+        printf("Elapsed: %g sec.\n", t.elapsed());
+    }
+#if 1
+    {
+        int N = 1000, K = 1000, M =1000;
+        
+        float *A = one(N*K);
+        print_as_matr(A, N, K, "A:");
+        float *B = one(K*M);
+        print_as_matr(B, K, M, "B:");
+        
+        float *C = new float[N*M];
+
+        Timer t;
+        t.start();
+        matr_mult(A, B, C, N, K, M);
+        t.stop();
+        print_as_matr(C, N, M, "C:");
+        printf("Elapsed: %g sec.\n", t.elapsed());
 
     }
+#endif
 }
 
 // Local Variables:
